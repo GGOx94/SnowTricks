b1ea308c6fd2b9067ebcb668afa61b4a6594c208
GGOx94 <mustbehax@hotmail.fr>
2022-09-13 16:52:29 +0200

    Password reset - Created mail login/reset_password_email template - Created twig login/reset_password template - Created new ResetPasswordController - Created new PasswordFormType

--- src/Form/PasswordFormType.php
+++ src/Form/PasswordFormType.php
@@ -2,48 +2,26 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
-class RegistrationFormType extends AbstractType
+class PasswordFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-
-            ->add('name', TextType::class, [
-                'label' => 'Nom d\'utilisateur',
-                'constraints' => [
-                    new Assert\NotBlank([ 'message' => 'Vous devez entrez un nom' ]),
-                    new Assert\Length([
-                        'min' => 3, 'minMessage' => 'Votre nom doit faire au moins {{ limit }} caractères.',
-                        'max' => 20, 'maxMessage' => 'Votre nom ne peut pas faire plus de {{ limit }} caractères.'
-                    ]),
-                ],
-            ])
-
-            ->add('email', EmailType::class, [
-                'constraints' => [
-                    new Assert\Email([ 'message' => "Le format de l'email n'est pas valide"])
-                ]
-            ])
-
-            //TODO use PasswordFormType
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'mapped' => false,
                 'invalid_message' => 'Les champs de mot de passe ne correspondent pas !',
                 'first_options' => [
-                     // instead of being set onto the object directly, this is read and encoded in the controller
+                    // instead of being set onto the object directly, this is read and encoded in the controller
                     'attr' => ['class' => 'password-field', 'label' => 'Mot de passe'],
                     'constraints' => [
                         new Assert\NotBlank([ 'message' => 'Vous devez entrez un mot de passe' ]),
@@ -60,14 +38,13 @@ class RegistrationFormType extends AbstractType
 
             ->add('submit', SubmitType::class, [
                 'attr' => [ 'class' => 'btn btn-success' ],
-                'label' => 'Créer un compte'
+                'label' => 'Changer mon mot de passe'
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
-    }
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//        $resolver->setDefaults([
+//        ]);
+//    }
 }
